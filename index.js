d9c9cfe5ad968b6b372dcad3765708480ab565ff
Oscar Urbina <im_toss@hotmail.com>
2019-07-18 17:07:15 -0500

    Instantiating Logger class into index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const Person = require('./person');
+const Logger = require('./reference/Logger');
+
+const logger = new Logger();
+logger.on('message', (data) => console.log('Called listener: ', data));
 
 const examplePerson = new Person('Oscar', 37);
 console.log(examplePerson.greeting());
+
+logger.log('Hello there!');
